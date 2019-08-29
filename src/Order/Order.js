5609b435f14c308287a21a8ff9cc6184c354d66b
Alan Turing <freemasl.sf@gmail.com>
2019-08-29 09:34:48 -0600

    Have each order display on the DOM

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -3,7 +3,13 @@ import './Order.css';
 
 const Order = ({order}) => {
   return (
-    <h2>{order.name}</h2>
+    <section>
+      <img src={order.img} alt={order.name} />
+      <h2>{order.name}</h2>
+      <p>{order.description}</p>
+      <p>${order.price}</p>
+      <button>Remove From History</button>
+    </section>
   )
 }
 
