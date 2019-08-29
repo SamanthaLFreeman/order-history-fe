a2274c47a0a31f53afbb4200916a34b3879037df
Alan Turing <freemasl.sf@gmail.com>
2019-08-29 09:52:17 -0600

    Have state update depending on what is typed in the form

--- src/OrdersForm/OrdersForm.js
+++ src/OrdersForm/OrdersForm.js
@@ -12,13 +12,33 @@ class OrdersForm extends Component {
     }
   }
 
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
   render() {
     return (
       <form>
-        <input placeholder="Image Url:" />
-        <input placeholder="Name:" />
-        <input placeholder="Description:" />
-        <input placeholder="Price:" />
+        <input 
+        placeholder="Image Url:" 
+        name="img" 
+        value={this.state.img} 
+        onChange={this.handleChange} />
+        <input 
+        placeholder="Name:" 
+        name="name" 
+        value={this.state.name} 
+        onChange={this.handleChange} />
+        <input 
+        placeholder="Description:" 
+        name="description" 
+        value={this.state.description}
+        onChange={this.handleChange} />
+        <input 
+        placeholder="Price:" 
+        name="price" 
+        value={this.state.price} 
+        onChange={this.handleChange} />
         <button>Add Purchase:</button>
       </form>
     )
